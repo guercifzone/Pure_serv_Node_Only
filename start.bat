ec8346d88deb5bb51a1fbd125bce92bdc75e42b1
sadikfattah <sadik.fattah@outlook.com>
2022-01-15 16:52:04 +0100

    only

---
+++ start.bat
@@ -0,0 +1,10 @@
+@echo
+rem node serv.js run the project in windows cmd
+set url="http://127.0.0.1:8081/"
+start chrome %url% 
+ cd C:\Work\nodejs\OS_MgM rem the path of the project
+ node server.js
+ rem nodemon server.js
+ rem loop:
+  rem rs
+  rem go to loop:
